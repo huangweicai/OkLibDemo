c514f85d333c47130b39e1b30611c2e806ec89ef
ex_huangwc <>
2017-08-06 20:07:30 +0800

    1.OkLib补充侧边栏及相关界面逻辑

--- app/src/main/java/com/oklib/demo/MainActivity.java
+++ app/src/main/java/com/oklib/demo/MainActivity.java
@@ -1,29 +1,43 @@
 package com.oklib.demo;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.oklib.demo.base.BaseAppActivity;
 
-/**
- * 时间：2017/8/5
- * 作者：黄伟才
- * 简书：http://www.jianshu.com/p/87e7392a16ff
- * github：https://github.com/huangweicai/oklib
- * 描述：首页功能展示入口
- */
 
-public class MainActivity extends BaseAppActivity {
+/**
+   * 时间：2017/8/1
+   * 作者：黄伟才
+   * 简书：http://www.jianshu.com/p/87e7392a16ff
+   * github：https://github.com/huangweicai/oklib
+   * 描述：入口类，功能列表展示
+   */
+public class MainActivity extends BaseAppActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
     private TabLayout toolbar_tl_tab;
     private ViewPager vp_container;
     private String[] titles = {"集成框架", "常用组件", "常用工具", "窗口相关"};
 
     @Override
     protected int initLayoutId() {
-        return R.layout.activiyt_mian;
+        return R.layout.activity_main;
     }
 
     @Override
@@ -38,6 +52,20 @@ public class MainActivity extends BaseAppActivity {
 
     @Override
     protected void initView() {
+        setStateBarStyle();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         toolbar_tl_tab = findView(R.id.toolbar_tl_tab);
         vp_container = findView(R.id.vp_container);
         toolbar_tl_tab.setupWithViewPager(vp_container);
@@ -64,5 +92,86 @@ public class MainActivity extends BaseAppActivity {
     @Override
     protected void initNet() {
 
+    }
+
+    private void setStateBarStyle() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {//5.0及以上
+            View decorView = getWindow().getDecorView();
+            int option =
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN//全屏
+                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;//状态栏字体深白色
+//                            | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;//状态栏字体深灰色（6.0以上属性）（注意：1.以最后一个设置为准 2.特定的颜色值才有效果）
+            decorView.setSystemUiVisibility(option);
+            //最好设置，不设置默认状态栏背景颜色是灰色
+            getWindow().setStatusBarColor(0x00ffffff);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {//4.4到5.0
+            WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
+            localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        if (id == R.id.exceptional_support) {
+            Intent intent = new Intent(this, PayActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.feedback_to_improve) {
+            Toast.makeText(this, "开发中···", Toast.LENGTH_LONG).show();
+        } else if (id == R.id.change_skin) {
+            Toast.makeText(this, "开发中···", Toast.LENGTH_LONG).show();
+        } else if (id == R.id.about_author) {
+            Toast.makeText(this, "开发中···", Toast.LENGTH_LONG).show();
+        } else if (id == R.id.jianshu) {//简书
+            Intent intent= new Intent();
+            intent.setAction("android.intent.action.VIEW");
+            Uri content_url = Uri.parse("http://www.jianshu.com/p/87e7392a16ff");
+            intent.setData(content_url);
+            startActivity(intent);
+        } else if (id == R.id.github) {//GitHub
+            Intent intent= new Intent();
+            intent.setAction("android.intent.action.VIEW");
+            Uri content_url = Uri.parse("https://github.com/huangweicai/oklib");
+            intent.setData(content_url);
+            startActivity(intent);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
---
+++ app/src/main/java/com/oklib/demo/PayActivity.java
@@ -0,0 +1,22 @@
+package com.oklib.demo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 时间：2017/8/2
+ * 作者：黄伟才
+ * 简书：http://www.jianshu.com/p/87e7392a16ff
+ * github：https://github.com/huangweicai/oklib
+ * 描述：
+ */
+
+public class PayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay);
+    }
+}
