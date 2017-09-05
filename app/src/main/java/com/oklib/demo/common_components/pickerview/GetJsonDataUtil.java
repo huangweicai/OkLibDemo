4c651834afa74edd44879ae444e97da2f5d7f0f2
ex_huangwc <>
2017-09-05 19:36:58 +0800

    添加pickerview

---
+++ app/src/main/java/com/oklib/demo/common_components/pickerview/GetJsonDataUtil.java
@@ -0,0 +1,36 @@
+package com.oklib.demo.common_components.pickerview;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 时间：2017/9/5
+ * 作者：黄伟才
+ * 简书：http://www.jianshu.com/p/87e7392a16ff
+ * github：https://github.com/huangweicai/OkLibDemo
+ * 描述：读取Json文件的工具类
+ */
+public class GetJsonDataUtil {
+
+    public String getJson(Context context, String fileName) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            AssetManager assetManager = context.getAssets();
+            BufferedReader bf = new BufferedReader(new InputStreamReader(
+                    assetManager.open(fileName)));
+            String line;
+            while ((line = bf.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stringBuilder.toString();
+    }
+}
+
